af15494d5eb24c8e0c6b55182aaf6db684d620d9
Valeria Zuñiga Mendoza <A01705435@tec.mx>
2024-03-10 10:12:01 -0600

    Eliminar lo de inicio de sesión

--- Lab12etc/controllers/users.controllers.js
+++
@@ -1,3 +0,0 @@
-exports.get_login = (request, response, next) => {
-    response.render('login');
-};
--- Lab12etc/routes/users.routes.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const usersController = require('../controllers/users.controller');
-router.get('/login', usersController.get_login);
-module.exports = router;
