22e111e28b8772877e38a8ca58aa680fec037dc3
Valeria Zuñiga Mendoza <A01705435@tec.mx>
2024-03-13 13:51:12 -0600

    Agregar protección de rutas

--- Lab12etc/routes/clases.routes.js
+++ Lab12etc/routes/clases.routes.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const isAuth = require('../util/is-auth');
 
 const productosController = require('../controllers/productos.controller');
 
-router.get('/agregarProducto', productosController.get_agregarProducto);
-router.post('/agregarProducto', productosController.post_agregarProducto);
-router.get('/catalogo/:IDProducto', productosController.get_catalogo);
-router.get('/catalogo', productosController.get_catalogo);
+router.get('/agregarProducto', isAuth, productosController.get_agregarProducto);
+router.post('/agregarProducto', isAuth, productosController.post_agregarProducto);
+router.get('/catalogo/:IDProducto', isAuth, productosController.get_catalogo);
+router.get('/catalogo', isAuth, productosController.get_catalogo);
 
 
 // Agregar respuesta a ruta raiz
-router.get('/',(request, response, next) => {
+router.get('/', isAuth, (request, response, next) => {
     response.render('pagInicio', {
         username: request.session.username || '',
     });
