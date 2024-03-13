91603e6abf06f99d97e254abf1e43b9edeb1e3fd
Valeria Zuñiga Mendoza <A01705435@tec.mx>
2024-03-13 13:14:58 -0600

    Cifrar contraseña al registrarse

--- Lab12etc/models/usuario.model.js
+++ Lab12etc/models/usuario.model.js
@@ -1,17 +1,25 @@
 const db = require('../util/database');
+const bcrypt = require('bcryptjs');
+
 module.exports = class Usuario {
-    //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
+    // Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
     constructor(mi_username, mi_nombre, mi_password) {
         this.username = mi_username;
         this.nombre = mi_nombre;
         this.password = mi_password;
     }
-    //Este método servirá para guardar de manera persistente el nuevo objeto. 
+    // Este método servirá para guardar de manera persistente el nuevo objeto. 
     save() {
-        return db.execute(
-            `INSERT INTO usuario (username, nombre, password) 
-            VALUES (?, ?, ?)`, 
-            [this.username, this.nombre, this.password]);
+        return bcrypt.hash(this.password, 12) // Encriptar la contraseña
+            .then((password_cifrado) => {
+                return db.execute(
+                    `INSERT INTO usuario (username, nombre, password) 
+                    VALUES (?, ?, ?)`, 
+                    [this.username, this.nombre, password_cifrado]);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
     static fetchOne(username) {
         return db.execute('Select * from usuario WHERE username = ?', [username]);
