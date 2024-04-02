c0aabc547cd53bfc4fec027443935ee1986b55f6
Valeria Zuñiga Mendoza <A01705435@tec.mx>
2024-04-02 17:46:56 -0600

    Agregar productos y actualizar script

--- Lab12etc/sql/tienda_online.sql
+++ Lab12etc/sql/tienda_online.sql
@@ -38,7 +38,7 @@ CREATE TABLE `asigna` (
 
 LOCK TABLES `asigna` WRITE;
 /*!40000 ALTER TABLE `asigna` DISABLE KEYS */;
-INSERT INTO `asigna` VALUES ('julieta',1),('Sofia',1),('raquel',2);
+INSERT INTO `asigna` VALUES ('julieta',1),('Sofia',1),('valeria',1),('raquel',2);
 /*!40000 ALTER TABLE `asigna` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -106,7 +106,7 @@ CREATE TABLE `producto` (
   `Precio` int DEFAULT NULL,
   `Imagen` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`IDProducto`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -115,7 +115,7 @@ CREATE TABLE `producto` (
 
 LOCK TABLES `producto` WRITE;
 /*!40000 ALTER TABLE `producto` DISABLE KEYS */;
-INSERT INTO `producto` VALUES (1,'Camisa blanca',449,'https://i.pinimg.com/474x/6d/f6/d5/6df6d5b1e9cd4167ad37b7a8557c1693.jpg'),(2,'Pantalón de vestir azul marino',749,'https://i.pinimg.com/474x/6f/af/e1/6fafe1996efa520a77372f8e01060ef6.jpg'),(3,'Traje negro',1599,'https://i.pinimg.com/474x/8c/e5/29/8ce529af57d3bc843755873c106d6361.jpg'),(4,'Pantalón de vestir beige',699,'https://i.pinimg.com/474x/4c/6b/5a/4c6b5ad613f217475971fa2aaa70a813.jpg'),(5,'Suéter blanco',449,'https://i.pinimg.com/474x/34/52/99/34529926f70b82e8e642df369aac3138.jpg'),(6,'Anillos y collar dorados',1150,'https://i.pinimg.com/474x/2b/00/c7/2b00c73c1f63dfef89a5605304f0337d.jpg'),(7,'Pulsera dorada',299,'https://i.pinimg.com/474x/66/22/a0/6622a0ff4b63f612a432a8c7f2ff3cf1.jpg'),(8,'Conjunto anillos',399,'https://i.pinimg.com/474x/d9/09/94/d909945d79dbd1affac05cca83d099d0.jpg'),(9,'Reloj dorado clásico',5600,'https://i.pinimg.com/474x/de/00/77/de00779390c2db197bbbb32c6472f5c1.jpg'),(10,'Saco negro de rayas',599,'https://i.pinimg.com/474x/fa/4d/88/fa4d88519d17f6db9b8fe830775ea4d9.jpg'),(11,'Brazalete + anillo',399,'https://i.pinimg.com/564x/77/ab/37/77ab37012723ac8fd303eae767666b4f.jpg');
+INSERT INTO `producto` VALUES (1,'Camisa blanca',449,'https://i.pinimg.com/474x/6d/f6/d5/6df6d5b1e9cd4167ad37b7a8557c1693.jpg'),(2,'Pantalón de vestir azul marino',749,'https://i.pinimg.com/474x/6f/af/e1/6fafe1996efa520a77372f8e01060ef6.jpg'),(3,'Traje negro',1599,'https://i.pinimg.com/474x/8c/e5/29/8ce529af57d3bc843755873c106d6361.jpg'),(4,'Pantalón formal beige',699,'https://i.pinimg.com/474x/4c/6b/5a/4c6b5ad613f217475971fa2aaa70a813.jpg'),(5,'Suéter blanco',449,'https://i.pinimg.com/474x/34/52/99/34529926f70b82e8e642df369aac3138.jpg'),(6,'Anillos y collar dorados',1150,'https://i.pinimg.com/474x/2b/00/c7/2b00c73c1f63dfef89a5605304f0337d.jpg'),(7,'Pulsera dorada',299,'https://i.pinimg.com/474x/66/22/a0/6622a0ff4b63f612a432a8c7f2ff3cf1.jpg'),(8,'Conjunto anillos',399,'https://i.pinimg.com/474x/d9/09/94/d909945d79dbd1affac05cca83d099d0.jpg'),(9,'Reloj dorado clásico',5600,'https://i.pinimg.com/474x/de/00/77/de00779390c2db197bbbb32c6472f5c1.jpg'),(10,'Saco negro de rayas',599,'https://i.pinimg.com/474x/fa/4d/88/fa4d88519d17f6db9b8fe830775ea4d9.jpg'),(11,'Brazalete + anillo',415,'https://i.pinimg.com/564x/77/ab/37/77ab37012723ac8fd303eae767666b4f.jpg'),(12,'Sueter gris',649,'684d76ac5fcb15d14e0105d4bac30e4d.jpg'),(13,'Saco rayado',455,'1712101133891sacorayado.jpg'),(14,'Pantalones de mezclilla claros',1299,'1712101171854pantalonesmezclillaclaros.jpg'),(15,'Conjunto de pulseras clips',399,'1712101199752conjuntopulseras.jpg');
 /*!40000 ALTER TABLE `producto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -164,9 +164,13 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES ('Daniel','Daniel Gutiérrez Hernández','$2a$12$MaWcLnPwuhgQh8JdmVx.v.7qtCHx5Bhkjp9pWMxKPMCL1zOKk3xVy'),('juan','Juan Luna García','verde11'),('julieta','Julieta Cantú Mendoza','$2a$12$Il8Mb6r.JSf3a9GbQYoFdOvAmtGXF0gq0SQxlS99cOYp6yMdpFFZ.'),('maria','María García García','azul99'),('raquel','Raquel Cantú Mendoza','$2a$12$UbuNiIbz7llnk0Js/rPzjuzwn7brvwEKX37D2K6xgo5tkykLBk2Ie'),('Sofia','Sofia López López','$2a$12$.J6xNnSP60XHpuh1csG2eujgi52dLBwc2qbqVRfPwLN.rN/M2BBOu'),('vale','Valeria Zúñiga Mendoza','contraseña123');
+INSERT INTO `usuario` VALUES ('Daniel','Daniel Gutiérrez Hernández','$2a$12$MaWcLnPwuhgQh8JdmVx.v.7qtCHx5Bhkjp9pWMxKPMCL1zOKk3xVy'),('juan','Juan Luna García','verde11'),('julieta','Julieta Cantú Mendoza','$2a$12$Il8Mb6r.JSf3a9GbQYoFdOvAmtGXF0gq0SQxlS99cOYp6yMdpFFZ.'),('maria','María García García','azul99'),('raquel','Raquel Cantú Mendoza','$2a$12$UbuNiIbz7llnk0Js/rPzjuzwn7brvwEKX37D2K6xgo5tkykLBk2Ie'),('Sofia','Sofia López López','$2a$12$.J6xNnSP60XHpuh1csG2eujgi52dLBwc2qbqVRfPwLN.rN/M2BBOu'),('vale','Valeria Zúñiga Mendoza','contraseña123'),('valeria','Valeria Zúñiga','$2a$12$OUaas1c0Wf9RW3ueyu2.6.ppdGocO8bhdBpy6jKbkc6vfzfCJ6M6K');
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'tienda_online'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -177,4 +181,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-03-18 16:40:45
+-- Dump completed on 2024-04-02 17:46:02
