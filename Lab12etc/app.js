83ec3dbdbb4f185c7a9fa027c6f8def565a42dfc
Valeria Zuñiga Mendoza <A01705435@tec.mx>
2024-04-11 14:03:47 -0600

    Cambios a lab 22

--- Lab12etc/app.js
+++ Lab12etc/app.js
@@ -20,6 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //Middleware
 app.use((request, response, next) => {
